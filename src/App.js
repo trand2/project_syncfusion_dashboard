646c698932a452044b95f304154a69b4bc35d935
Tyler Randolph <trandolph@clearcenter.com>
2022-05-25 14:32:50 -0600

    update homepage

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ const App = () => {
                         <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                             <Navbar />
                         </div>
-                    </div>
 
                     <div>
                         <Routes>
@@ -69,7 +68,7 @@ const App = () => {
 
                         </Routes>
                     </div>
-
+                </div>
                 </div>
             </BrowserRouter>
         </div>
