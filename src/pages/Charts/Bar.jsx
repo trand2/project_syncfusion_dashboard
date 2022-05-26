99033f94bd436d7069ce9b243c3e5ff4fca826ad
Tyler Randolph <trandolph@clearcenter.com>
2022-05-26 11:42:29 -0600

    Add different styles of charts

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Tooltip, ColumnSeries, DataLabel, Category } from '@syncfusion/ej2-react-charts';
 
-const Bar = () => {
+import { Header } from '../../components';
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const BarChart = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Bar</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Bar" title="Olympic Medal Counts - RIO"/>
+      <ChartComponent
+        id='bar-chart'
+        height='420px'
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 }}}
+        tooltip={{ enable: true }}
+        background={ currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[Tooltip, Legend, ColumnSeries, DataLabel, Category ]}/>
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} /> )}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
-export default Bar
+export default BarChart
--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Tooltip, ColumnSeries, Category, RangeColorSettingDirective, RangeColorSettingsDirective } from '@syncfusion/ej2-react-charts';
 
-const ColorMapping = () => {
+import { Header } from '../../components';
+import { colorMappingData, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis, rangeColorMapping } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+const ColorMappingChart = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>ColorMapping</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Color Mappping" title="USA CLIMATE - WEATHER BY MONTH"/>
+      <ChartComponent
+        id='colormapping-chart'
+        height='420px'
+        primaryXAxis={ColorMappingPrimaryXAxis}
+        primaryYAxis={ColorMappingPrimaryYAxis}
+        chartArea={{ border: { width: 0 }}}
+        tooltip={{ enable: true }}
+        legendSettings={{ mode: 'Range', background: 'white' }}
+        background={ currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[ ColumnSeries, Tooltip, Category, Legend ]}/>
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={colorMappingData[0]}
+            name="USA"
+            xName="x"
+            yName="y"
+            type="Column"
+            cornerRadius={{
+              topLeft: 10,
+              topRight: 10,
+            }}
+          />
+        </SeriesCollectionDirective>
+        <RangeColorSettingsDirective>
+          {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item} /> )}
+        </RangeColorSettingsDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
-export default ColorMapping
+export default ColorMappingChart
